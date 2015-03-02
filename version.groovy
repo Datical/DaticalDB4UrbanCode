a34d1c639e07f26cf59ea6746979722334e97bc8
r2datical <r2@datical.com>
2015-03-02 16:15:19 -0600

    Add support for "show version"

---
+++ version.groovy
@@ -0,0 +1,55 @@
+import com.urbancode.air.CommandHelper;
+
+final def inputPropsFile = new File(args[0])
+final def outputPropsFile = new File(args[1])
+
+final def props = new Properties()
+try {
+    props.load(new FileInputStream(inputPropsFile))
+}
+catch (IOException e) {
+    throw new RuntimeException(e)
+}
+
+final def cwd = new File('.');
+final def cmdHelper = new CommandHelper(cwd);
+
+
+
+//--------------------------------------------------------------------------------------------------
+def getAbsPath(def file) {
+    def tempFile = null;
+    if (file != null && file != "") {
+        File temporaryFile = new File(file);
+        tempFile = temporaryFile.getAbsolutePath();
+    }
+    return tempFile;
+}
+//path properties
+def daticalDBCmd = getAbsPath(props['daticalDBCmd']);
+def daticalDBDriversDir = getAbsPath(props['daticalDBDriversDir']);
+def daticalDBvm = props['daticalDBvm'];
+def daticalDBvmargs = props['daticalDBvmargs'];
+
+def cmdArgs = cmdArgs = [daticalDBCmd, '-drivers', daticalDBDriversDir];
+
+cmdArgs << "show";
+cmdArgs << "version";
+
+if (daticalDBvm) {
+	cmdArgs << "--vm";
+	cmdArgs << daticalDBvm;
+}
+
+if (daticalDBvmargs) {
+	cmdArgs << "--vmargs";
+	String[] myArray = daticalDBvmargs.split();
+	for ( x in myArray ) {
+		cmdArgs << x;
+	}
+	//cmdArgs << daticalDBvmargs;
+}
+
+int exitCode = cmdHelper.runCommand("Executing Datical DB", cmdArgs);
+
+System.exit(exitCode);
